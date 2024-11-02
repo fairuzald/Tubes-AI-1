917d64af3164903de6c2997515e0f8c8de7e1287
fairuzald <fairuzy210@gmail.com>
2024-11-03 00:41:47 +0700

    feat: add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ import { initialState, reducer } from "@/utils/reducer";
 import { useInterval } from "@/hooks/useInterval";
 import { SearchDto } from "@/lib/SearchDto";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function MagicCubeSolver() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -106,13 +107,16 @@ export default function MagicCubeSolver() {
     }
     router.refresh();
     dispatch({ type: "RESET_STATE" });
-  }, []);
+  }, [router]);
 
   return (
-    <main className="px-8 sm:px-10 md:px-14 lg:px-16 2xl:px-20 py-10">
-      <h1 className="text-center text-3xl font-bold mb-10">
-        Diagonal Magic Cube Solver
-      </h1>
+    <main className="px-8 sm:px-10 md:px-14 lg:px-16 xl:px-28 py-10">
+      <div className="flex gap-4 items-center justify-center mb-10">
+        <Image src="/logo.png" alt="Magic Cube" width={50} height={50} />
+        <h1 className="text-center text-3xl font-bold">
+          Diagonal Magic Cube Solver
+        </h1>
+      </div>
 
       <div className="m-auto flex flex-col gap-4 items-center justify-center">
         <div className="grid grid-cols-2 gap-4">
