fce46333194a95cc5c9d84d6aa9d58961900f1c0
Rayhan Fadhlan <rayhanfazka07@gmail.com>
2024-11-02 14:28:20 +0700

    feat : steepest ascent, sideways, stochastic

---
+++ src/lib/SidewaysMove.ts
@@ -0,0 +1,45 @@
+import { LocalSearch } from "./LocalSearch";
+
+export class SidewaysMove extends LocalSearch {
+    private cube : MagicCube;
+
+    constructor(cube:MagicCube){
+        super(cube);
+        this.cube = cube;
+
+    }
+
+    public solve(maxSideways : number = 100) : void {
+
+        this.startTimer();
+
+        let currentState = this.cube;
+        let currentSideways = 0;
+
+        while(currentState.calculateObjectiveFunction() !=  0 && currentSideways < maxSideways){
+            const nextState = currentState.getBestSuccessor();
+
+            const currentObj = currentState.calculateObjectiveFunction();
+            const nextObj = nextState.calculateObjectiveFunction();
+
+            const deltaE = nextObj - currentObj;
+
+            this.addStateEntry(currentState);
+            this.addObjectiveFunctionPlotEntry(this.iterationCount, currentObj);
+
+            if(deltaE >= 0){
+                if(deltaE == 0){
+                    currentSideways++;
+                }
+                this.addIterationCount();
+                currentState = nextState;
+                
+            }
+            else{
+                break;
+            }
+
+        }
+        this.endTimer();
+    }
+}
